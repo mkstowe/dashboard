e1b4b21910728e72e2b9218b20a7f9b64ff64756
Michael Stowe <michaelstowe12@gmail.com>
2023-06-21 00:34:22 -0400

    Testing

--- frontend/functions/hass/history.js
+++ frontend/functions/hass/history.js
@@ -1,14 +1,15 @@
 export async function onRequest(context) {
   const entityId = context.request.body.entityId;
-  const response = await fetch(
-    `${context.env.HASS_URL}/api/history/period?filter_entity_id=${entityId}`,
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${context.env.HASS_TOKEN}`
-      }
-    }
-  );
+  // const response = await fetch(
+    // `${context.env.HASS_URL}/api/history/period?filter_entity_id=${entityId}`,
+    // {
+      // headers: {
+        // 'Content-Type': 'application/json',
+        // Authorization: `Bearer ${context.env.HASS_TOKEN}`
+      // }
+    // }
+  // );
 
-  return new Response(response);
+  // return new Response(response);
+  return new Response(`BODY: ${context.request.body}, \nURL: ${context.request.url}, \nREQUEST: ${context.request}`);
 }
