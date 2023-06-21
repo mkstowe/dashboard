0f35cbf2094c1d1fe1539f2e009954c023a5749f
Michael Stowe <michaelstowe12@gmail.com>
2023-06-21 18:00:45 -0400

    Maybe?

--- frontend/functions/hass/history.js
+++ frontend/functions/hass/history.js
@@ -1,17 +1,14 @@
 export async function onRequest(context) {
-  // const queryString = url.get('entity');
   const entityId = context.request.url.split('=')[1];
-  return new Response(entityId);
-  // const response = await fetch(
-    // `${context.env.HASS_URL}/api/history/period?filter_entity_id=${entityId}`,
-    // {
-      // headers: {
-        // 'Content-Type': 'application/json',
-        // Authorization: `Bearer ${context.env.HASS_TOKEN}`
-      // }
-    // }
-  // );
+  const response = await fetch(
+    `${context.env.HASS_URL}/api/history/period?filter_entity_id=${entityId}`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${context.env.HASS_TOKEN}`
+      }
+    }
+  );
 
-  // return new Response(response);
-  // return new Response(`BODY: ${context.request.body}, \nJSON: ${context.request.json}, \nURL: ${context.request.url}, \nREQUEST: ${JSON.stringify(context.request)}`);
+  return new Response(response);
 }
