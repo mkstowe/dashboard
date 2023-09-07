6cf7a5639b9a7cf2770f2abe1888a7471595e72b
Michael Stowe <michaelstowe12@gmail.com>
2023-09-07 17:16:31 -0400

    Sync

--- frontend/src/app/recipes/pages/recipe-page/recipe-page.component.ts
+++ frontend/src/app/recipes/pages/recipe-page/recipe-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { RecipeService } from '../../services/recipe.service';
-import { take } from 'rxjs';
+import { BehaviorSubject, Observable, take } from 'rxjs';
 import { PageEvent } from '@angular/material/paginator';
 
 @Component({
@@ -15,30 +15,39 @@ export class RecipePageComponent implements OnInit {
   public pageSize = 25;
   public pageSizeOptions = [5, 10, 25, 50, 100];
   public recipes: any[];
+  public allRecipes: Observable<any[]>;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService) {
+  }
 
   public ngOnInit(): void {
-    this.recipeService
-      .getRecipes(this.page, this.pageSize)
-      .pipe(take(1))
-      .subscribe((res: any) => {
-        this.recipes = res?.items;
-        this.numRecipes = res?.total;
-        this.numPages = res?.total_pages;
-      });
+    this.allRecipes = this.recipeService.recipes;
+    // this.recipeService.recipes.subscribe((res: any) => {
+    //   this.allRecipes = res;
+    // })
+    // this.recipes = this.allRecipes.slice(0, this.pageSize);
+    // this.recipeService.recipes.subscribe((res: any) => {
+      // this.allRecipes = res;
+      // this.recipes = res.split(0, this.pageSize);
+      // console.log(this.allRecipes)
+      // this.numRecipes = this.allRecipes.length;
+    // })
+    // this.recipeService
+    //   .getRecipes()
+    //   .pipe(take(1))
+    //   .subscribe((res: any) => {
+    //     this.allRecipes = res;
+    //     this.recipes = this.allRecipes.slice(0, this.pageSize);
+    //     this.numRecipes = res.length;
+    //     this.numPages = Math.ceil(this.numRecipes / this.pageSize);
+    //   });
   }
 
   public onPage($event: PageEvent) {
     this.page = $event.pageIndex + 1;
     this.pageSize = $event.pageSize;
 
-    this.recipeService
-      .getRecipes(this.page, this.pageSize)
-      .pipe(take(1))
-      .subscribe((res: any) => {
-        this.recipes = res?.items;
-      });
+
   }
 
   public trackRecipe(index: number, recipe: any) {
--- frontend/src/app/recipes/services/recipe.service.ts
+++ frontend/src/app/recipes/services/recipe.service.ts
@@ -1,16 +1,27 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, EMPTY, expand, reduce } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class RecipeService {
+  private _recipesSource = new BehaviorSubject<any>(null);
+
   private headers = {
     Authorization: `Bearer ${environment.mealieAccessToken}`,
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.getRecipes().subscribe((res) => {
+      this._recipesSource.next(res)
+    });
+  }
+
+  public get recipes() {
+    return this._recipesSource.asObservable();
+  }
 
   public getRecipe(slug: string) {
     return this.http.get(`/api/mealie/recipes/${slug}`, {
@@ -18,12 +29,20 @@ export class RecipeService {
     });
   }
 
-  public getRecipes(page: number, perPage: number) {
-    return this.http.get(
-      `/api/mealie/recipes?page=${page}&perPage=${perPage}`,
-      {
+  public getRecipes() {
+    return this.http
+      .get(`/api/mealie/recipes?orderBy=name&orderDirection=asc`, {
         headers: this.headers,
-      }
-    );
+      })
+      .pipe(
+        expand((res: any) => {
+          return res.next
+            ? this.http.get(`/api/mealie/${res.next}`, {
+                headers: this.headers,
+              })
+            : EMPTY;
+        }),
+        reduce((acc, curr: any) => acc.concat(curr.items), [])
+      );
   }
 }
