a119ef972f6b254ab1e918a6bf4f6f82318801af
Michael Stowe <michaelstowe12@gmail.com>
2024-04-24 16:33:52 -0400

    Env update

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+
+    export const environment = {
+      production: false,
+      environment: 'development',
+      apiUrl: 'http://localhost:3000',
+      hassUrl: 'https://hass.mkstowe.com',
+      mealieUrl: 'https://recipes.mkstowe.com',
+    };
+  
---
+++ frontend/src/environments/environment.production.ts
@@ -0,0 +1,9 @@
+
+    export const environment = {
+      production: true,
+      environment: 'production',
+      apiUrl: 'http://localhost:3000',
+      hassUrl: 'https://hass.mkstowe.com',
+      mealieUrl: 'https://recipes.mkstowe.com',
+    };
+  
